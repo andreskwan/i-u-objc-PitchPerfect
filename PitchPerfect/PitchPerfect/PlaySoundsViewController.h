09199d45f1d5ede3cebdc86e0983c67e6ab60110
ANDRES KWAN <andreskwan@gmail.com>
2015-08-30 11:29:00 -0500

    Add - PlaySoundsVC class - PlaySoundsVC view - playSlowButton - constrains for button

---
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.h
@@ -0,0 +1,14 @@
+//
+//  PlaySoundsViewController.h
+//  PitchPerfect
+//
+//  Created by Andres Kwan on 8/30/15.
+//  Copyright (c) 2015 Kwan. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PlaySoundsViewController : UIViewController
+- (IBAction)playSlowButton:(UIButton *)sender;
+
+@end
---
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.m
@@ -0,0 +1,39 @@
+//
+//  PlaySoundsViewController.m
+//  PitchPerfect
+//
+//  Created by Andres Kwan on 8/30/15.
+//  Copyright (c) 2015 Kwan. All rights reserved.
+//
+
+#import "PlaySoundsViewController.h"
+
+@interface PlaySoundsViewController ()
+
+@end
+
+@implementation PlaySoundsViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+- (IBAction)playSlowButton:(UIButton *)sender {
+}
+@end
