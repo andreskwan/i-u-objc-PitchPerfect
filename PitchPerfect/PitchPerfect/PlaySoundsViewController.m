7f708ba378f0c33da0837bf31a9e2d90e0094974
ANDRES KWAN <andreskwan@gmail.com>
2015-08-30 12:49:11 -0500

    Add - audio file to be played - logic to reproduce sound in playSoundVC->playSlowButton

--- PitchPerfect/PitchPerfect/PlaySoundsViewController.m
+++ PitchPerfect/PitchPerfect/PlaySoundsViewController.m
@@ -7,9 +7,11 @@
 //
 
 #import "PlaySoundsViewController.h"
+@import AVFoundation;
+
 
 @interface PlaySoundsViewController ()
-
+@property (nonatomic, strong) AVAudioPlayer *audioPlayer;
 @end
 
 @implementation PlaySoundsViewController
@@ -17,6 +19,7 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view.
+    [self configureAudioToPlay];
 }
 
 - (void)didReceiveMemoryWarning {
@@ -24,16 +27,37 @@
     // Dispose of any resources that can be recreated.
 }
 
-/*
-#pragma mark - Navigation
-
-// In a storyboard-based application, you will often want to do a little preparation before navigation
-- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
-    // Get the new view controller using [segue destinationViewController].
-    // Pass the selected object to the new view controller.
-}
-*/
-
 - (IBAction)playSlowButton:(UIButton *)sender {
+    [self.audioPlayer play];
 }
+
+#pragma mark Audio 
+- (void)configureAudioToPlay
+{
+    NSString *path = [[NSBundle mainBundle] pathForResource:@"movie_quote"
+                                                     ofType:@"mp3"];
+    NSURL *urlPath = [NSURL fileURLWithPath:path];
+    NSError *error;
+    //TODO: identify why it is nil
+    self.audioPlayer = [[AVAudioPlayer alloc]initWithContentsOfURL:urlPath
+                                                             error:&error];
+    //TODO:handle this error - read
+    //https://developer.apple.com/library/ios/documentation/Cocoa/Conceptual/ErrorHandlingCocoa/ErrorHandling/ErrorHandling.html#//apple_ref/doc/uid/TP40001806-CH201-SW1
+    if (!error) {
+        
+    }else{
+        //TODO: handle error
+    }
+}
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
 @end
