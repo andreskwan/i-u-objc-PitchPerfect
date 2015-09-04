5fcae077f35a8110adba16e00958394dfaddec72
ANDRES KWAN <andreskwan@gmail.com>
2015-09-04 06:00:25 -0500

    Change - name of the parameters of the AVAudioRecorded initializer - validate audio session SetCategory, SetActive add - identifier to the segue - stopRecording TODO - remove ! from flag

--- PitchPerfect/PitchPerfect/RecordSoundViewController.m
+++ PitchPerfect/PitchPerfect/RecordSoundViewController.m
@@ -48,70 +48,80 @@ static NSString *const kSegueIdentifierForStopRecording = @"stopRecording";
     //TODO: Record the user's voice
     //optain the path to the Documents directory
     //---------Path & Name of the file
-    NSString *dirPath = [NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES) firstObject];
+    NSString *outputDirPath = [NSSearchPathForDirectoriesInDomains(NSDocumentationDirectory, NSUserDomainMask, YES) firstObject];
     NSString *recordingName = [kAudioFileName stringByAppendingPathExtension:kAudioFileExtension];
-    NSArray *pathArray = @[dirPath, recordingName];
-    NSURL *filePath = [NSURL fileURLWithPathComponents:pathArray];
+    NSArray *pathArray = @[outputDirPath, recordingName];
+    NSURL *outputFilePath = [NSURL fileURLWithPathComponents:pathArray];
 
     //---------Record Audio
-    /* example of the audio settings dictionary
+    /* example of the audio settings dictionary    */
     //http://www.techotopia.com/index.php/Recording_Audio_on_iOS_7_with_AVAudioRecorder
-    NSDictionary *recordSettings = [NSDictionary dictionaryWithObjectsAndKeys: [NSNumber numberWithInt:AVAudioQualityMin],
-                                    AVEncoderAudioQualityKey,
-                                    [NSNumber numberWithInt:16],
-                                    AVEncoderBitRateKey,
-                                    [NSNumber numberWithInt: 2],
-                                    AVNumberOfChannelsKey,
-                                    [NSNumber numberWithFloat:44100.0],
-                                    AVSampleRateKey,
-                                    nil];
-    */
+//    NSDictionary *recordSettings = [NSDictionary dictionaryWithObjectsAndKeys: [NSNumber numberWithInt:AVAudioQualityMin],
+//                                    AVEncoderAudioQualityKey,
+//                                    [NSNumber numberWithInt:16],
+//                                    AVEncoderBitRateKey,
+//                                    [NSNumber numberWithInt: 2],
+//                                    AVNumberOfChannelsKey,
+//                                    [NSNumber numberWithFloat:44100.0],
+//                                    AVSampleRateKey,
+//                                    nil];
+
     AVAudioSession * audioSession = [AVAudioSession sharedInstance];
-    NSError *errorAudioSession = nil;
-    NSError *errorAudioRecorder = nil;
+//    NSError *errorAudioSession = nil;
+//    NSError *errorAudioRecorder = nil;
     //should be set before activating the session
     //what means to activate the audio session?
     //what is the category?
     //what is the mode?
     //are the same?
-    [audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
-                        error:&errorAudioSession];
-    
-    
-    self.audioRecorder = [[AVAudioRecorder alloc]initWithURL:filePath
-                                                    settings:nil
-                                                       error:&errorAudioRecorder];
-    self.audioRecorder.delegate = self;
-    self.audioRecorder.meteringEnabled = YES;
-    [self.audioRecorder record];
+    if ([audioSession setCategory:AVAudioSessionCategoryPlayAndRecord
+                            error:nil]) {
+        self.audioRecorder = [[AVAudioRecorder alloc]initWithURL:outputFilePath
+                                                        settings:nil
+                                                           error:nil];
+        
+        self.audioRecorder.delegate = self;
+        self.audioRecorder.meteringEnabled = YES;
+        [self.audioRecorder record];
+    }else{
+        
+    }
 }
 
 - (IBAction)stopRecord:(UIButton *)sender {
     self.recordLabel.hidden = YES;
     [self.audioRecorder stop];
+    
     AVAudioSession * audioSession = [AVAudioSession sharedInstance];
     NSError *errorAudioSession = nil;
-    [audioSession setActive:NO
-                      error:&errorAudioSession];
+    if ([audioSession setActive:NO
+                          error:&errorAudioSession]) {
+        NSLog(@"%@",@"session deactivated");
+    }else{
+        NSLog(@"%@",@"session cant be deactivated");
+    }
 }
 #pragma mark Segues
 -(void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
 {
-    
+    if (segue.identifier == kSegueIdentifierForStopRecording) {
+        UIViewController *playSoundVC = [segue destinationViewController];
+    }
 }
 
 #pragma mark AVAudioRecorderDelegate
 -(void)audioRecorderDidFinishRecording:(AVAudioRecorder *)recorder
                           successfully:(BOOL)flag
 {
-    if (flag) {
+    if (!flag) {
         //TODO: save the recorded audio
         self.recordedAudio = [RecordedAudio new];
-        self.recordedAudio.filePathUrl = self.audioRecorder.url.lastPathComponent;
-        //TODO: move to the next scene aka perform segue
-        //sender means the UIVC from which the segue depart
+        self.recordedAudio.filePathUrl = recorder.url;
+        self.recordedAudio.title = recorder.url.lastPathComponent;
+        //TODO: move  the next scene aka perform segue
+        //sender is the object that actually initiates the segue
         [self performSegueWithIdentifier:kSegueIdentifierForStopRecording
-                                  sender: self.recordedAudio];
+                                  sender:self.recordedAudio];
     }else{
         self.recordButton.enabled = YES;
         self.stopButton.hidden = YES;
